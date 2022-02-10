9b9e985475489876a10850ee2cf9ccf47a7dfc2d
Jeet Z. H. Khondker <48243450+jeet-khondker@users.noreply.github.com>
2022-02-11 07:42:00 +0900

    JavaScript Import & Export

---
+++ Import-Export/data.js
@@ -0,0 +1,1 @@
+export const add = (number1, number2) => number1 + number2;
---
+++ Import-Export/index.js
@@ -0,0 +1,15 @@
+import { user } from "./app.js";
+
+let updateData = user;
+
+updateData.push("Shahin");
+
+console.log(updateData);
+
+/* Challenge */
+
+import { add } from "./data.js";
+
+let result = add(10, 40);
+
+console.log(result);
